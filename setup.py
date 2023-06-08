15eb0e56d2c92b857aee25e9aca0ad5e41a7e4fa
aj088 <55105250+aj088@users.noreply.github.com>
2023-06-08 10:21:41 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scATAnno-main',
+    version='1.0',
+    packages=['scATAnno'],
+    url='https://github.com/aj088/scATAnno-main/',
+    license='',
+    author='Yijia Jiang',
+    author_email='yijia_jiang@dfci.harvard.edu',
+    description=''
+)
