ca4172e4a545b2c017e980837536f4ff19f4affb
aj088 <55105250+aj088@users.noreply.github.com>
2023-06-13 13:39:12 -0400

    edit setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='scATAnno',
-    version='1.0.1',
+    version='1.0.2',
     packages=['scATAnno'],
     url='https://github.com/aj088/scATAnno-main/',
     license='',
     author='Yijia Jiang',
     author_email='yijia_jiang@dfci.harvard.edu',
     description='',
-    install_requires=['pandas','scanpy','anndata','matplotlib','adjustText', 'leidenalg', 'harmonypy', 'scipy','seaborn'],
+    install_requires=['pandas','scanpy','anndata','matplotlib','adjustText', 'leidenalg', 'harmonypy', 'scipy','seaborn', "snapatac2"],
 )
