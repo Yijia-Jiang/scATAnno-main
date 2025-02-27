3046b6de7936a069636f2dbda846e7d83824586b
FrancisFengLu <75783773+FrancisFengLu@users.noreply.github.com>
2025-02-27 10:44:17 -0500

    Update generate_atlas.py
    Add true_label path

--- prep_data/scATAnno-reference-building-example/generate_atlas.py
+++ prep_data/scATAnno-reference-building-example/generate_atlas.py
@@ -23,33 +23,40 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--countMatrixLocation', help="peak count matrix location")
+parser.add_argument('--celllabel', help="path to cell label file")
 args = parser.parse_args()
-path=args.countMatrixLocation
+path = args.countMatrixLocation
+cell_label_path = args.celllabel
 
-
-def convert_mtx2anndata_quickATAC(path, mtx_file,cells_file,features_file, variable_prefix, major_col =True ,minor_col = True, sep=(":", "-"),add_metrics = True):
+def convert_mtx2anndata_quickATAC(path, mtx_file, cells_file, features_file, variable_prefix, major_col=True, minor_col=True, sep=(":", "-"), add_metrics=True, cell_label_path=None):
     # feature peak must be sep by (":","-")
-    data = sc.read_mtx(os.path.join(path,mtx_file))
+    data = sc.read_mtx(os.path.join(path, mtx_file))
     data = data.T
-    features = pd.read_csv(os.path.join(path, features_file), header=None, sep= '\t')
+    features = pd.read_csv(os.path.join(path, features_file), header=None, sep='\t')
     barcodes = pd.read_csv(os.path.join(path, cells_file), header=None)
 
     # Split feature matrix and set peak separated by (:, -) to match reference peaks
     data.var_names = features[0]
     data.obs_names = barcodes[0]
-    
+
     if major_col == True:
         data.obs[major_col] = variable_prefix
     if minor_col == True:
         data.obs[minor_col] = variable_prefix
     if (major_col == False) and (minor_col == False):
         data.obs["cell type"] = variable_prefix
-    
+
     data.obs['tissue'] = variable_prefix
     data.obs['dataset'] = variable_prefix
-    
+
     # remove spike-in cell
     data = data[data.obs.index != "spike-in"]
+    
+    # add cell labels if provided
+    if cell_label_path is not None:
+        cell_labels = pd.read_csv(cell_label_path, index_col=0)
+        data.obs = pd.merge(data.obs, cell_labels[['true_lable']], left_index=True, right_index=True, how='left')
+    
     # add qc filtering metrics from quickATAC if add_metrics set to true
     if add_metrics == True:
         import glob
@@ -57,7 +64,7 @@ def convert_mtx2anndata_quickATAC(path, mtx_file,cells_file,features_file, varia
             metrics_filepath = glob.glob(os.path.join(path, "*meta*"))[0]
             metrics = pd.read_csv(metrics_filepath, sep='\t', index_col=0)
             metrics = metrics[metrics.index != "spike-in"]
-            data.obs = pd.merge(data.obs, metrics, right_index=True, left_index = True)
+            data.obs = pd.merge(data.obs, metrics, right_index=True, left_index=True)
         except OSError as error:
             import warnings
             warnings.warn('Metrics file not found, anndata returned with no meta metrics')
@@ -65,7 +72,9 @@ def convert_mtx2anndata_quickATAC(path, mtx_file,cells_file,features_file, varia
     return data
 
 
-reference_atlas = convert_mtx2anndata_quickATAC(path, mtx_file = "matrix.mtx.gz",
-                              cells_file = "barcodes.tsv",features_file ="features.tsv", variable_prefix ="Atlas", major_col=False,minor_col=False)
+reference_atlas = convert_mtx2anndata_quickATAC(path, mtx_file="matrix.mtx.gz",
+                              cells_file="barcodes.tsv", features_file="features.tsv", 
+                              variable_prefix="Atlas", major_col=False, minor_col=False,
+                              cell_label_path=cell_label_path)
 
-reference_atlas.write(os.path.join(path,"Reference_Built.h5ad"))
+reference_atlas.write(os.path.join(path, "Reference_Built.h5ad"))
